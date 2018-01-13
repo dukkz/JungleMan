bb72e35c59bfe63f32b05ce17d2252ecb5f3078b
Jacob <dukkz24@gmail.com>
2018-01-13 13:32:31 -0600

    First Publish
    I did a thing.

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+	public Transform target;
+
+	public Vector3 offset;
+
+	public float pitch = 15f, zoomSpeed = 2f, minZoom = 3f, maxZoom = 10f;
+
+	private float currentZoom = 10f;
+
+	void Update(){
+		currentZoom -= Input.GetAxis ("Mouse ScrollWheel") * zoomSpeed;
+		currentZoom = Mathf.Clamp (currentZoom, minZoom, maxZoom);
+	}
+
+	void LateUpdate(){
+		transform.position = target.position - offset * currentZoom;
+		transform.LookAt (target.position + Vector3.up * pitch);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+
+	public LayerMask movementMask;
+
+	Camera camera;
+	PlayerMotor motor;
+
+	// Use this for initialization
+	void Start () {
+		camera = Camera.main;
+		motor = GetComponent<PlayerMotor> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetMouseButton (0)) {
+			Ray ray = camera.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+
+			if (Physics.Raycast (ray, out hit, 1000, movementMask)) {
+				motor.MoveToPoint (hit.point);
+				//Stop Focusing Object
+			}
+		}
+		if (Input.GetMouseButton (1)) {
+			Ray ray = camera.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+
+			if (Physics.Raycast (ray, out hit, 1000)) {
+				// Check if we hit an interactable
+				//If so, set as focus
+			}
+		}
+	}
+}
